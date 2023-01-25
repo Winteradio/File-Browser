a6e9c996e663557add21c73d39ed3fb347eb5849
Winteradio <tot4766@hanyang.ac.kr>
2023-01-26 04:08:29 +0900

    Type : feature Branch : BST Auther : Lee Wonmock Date : 2023-01-26 Desc : Make BST Framework

---
+++ src/SORT/BST.cpp
@@ -0,0 +1,51 @@
+#include "BST.h"
+
+void BST::Insert( int Value )
+{
+	
+}
+
+void BST::Delete( int Value )
+{
+	
+}
+
+void BST::Search( int Value, TraversalType Type )
+{
+	switch( Type )
+	{
+		case INORDER : InorderTraversal();
+		break;
+
+		case PREORDER : PreorderTraversal();
+		break;
+
+		case POSTORDER : PostorderTraversal();
+		break;
+	};
+}
+
+void BST::InorderTraversal()
+{
+	
+}
+
+void BST::PreorderTraversal()
+{
+	
+}
+
+void BST::PostorderTraversal()
+{
+	
+}
+
+void BST::GetSuccesor()
+{
+	
+}
+
+void BST::GetPredecessor()
+{
+	
+}
--- src/SORT/BST.h
+++ src/SORT/BST.h
@@ -4,5 +4,28 @@
 #include "SUB_COM.h"
 #include "Node.h"
 
+typedef enum TraversalType
+{
+	INORDER,
+	PREORDER,
+	POSTORDER
+}TraversalType;
+
+class BST
+{
+	public :
+		void Insert( int Value );
+		void Delete( int Value );
+		void Search( int Value, TraversalType Type );
+
+		void InorderTraversal();
+		void PreorderTraversal();
+		void PostorderTraversal();
+
+		void GetSuccesor();
+		void GetPredecessor();
+
+
+};
 
 #endif // __BST_H__
